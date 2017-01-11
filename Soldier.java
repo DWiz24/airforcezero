ec4a6080f70ac7726500ed4a5649d10b8dcee4ef
DWiz24 <bobthequark@gmail.com>
2017-01-11 15:06:27 -0500

    work

--- Soldier.java
+++ Soldier.java
@@ -52,11 +52,14 @@ public class Soldier {
                             treeOrNot=false;
                         }
                     }
+                    if (closest==999f) {
+                        bugging=false;
+                    }
                 }
             }
             if (bugging) {
-                float dist=rc.getLocation().add(toDest,2).distanceTo(dest);
-                if (rc.canMove(toDest)&&dist<bugMinDist) {
+                float destdist=rc.getLocation().add(toDest,2).distanceTo(dest);
+                if (rc.canMove(toDest)&&destdist<bugMinDist) {
                     bugging=false;
                     rc.move(toDest);
                 } else {
@@ -65,7 +68,36 @@ public class Soldier {
                         bugging=false;
                     } else {
                         float distBtw=rc.getLocation().distanceTo(following.getLocation());
-                        Direction ndir=new Direction(rc.getLocation().directionTo(following.getLocation()).radians-(float)Math.acos((distBtw*distBtw+4-(following.getRadius()+1)*(following.getRadius()+1))));
+                        Direction ndir=new Direction(rc.getLocation().directionTo(following.getLocation()).radians-0.0001f-(float)Math.acos((distBtw*distBtw+4-(following.getRadius()+1)*(following.getRadius()+1))));
+                        if (rc.canMove(ndir)) {
+                            rc.move(ndir);
+                        } else {
+                            bugging = true;
+                            bugMinDist = distToDest;
+                            MapLocation move = rc.getLocation().add(ndir, 2);
+                            float closest = 999;
+                            for (int i = trees.length - 1; i >= 0; i--) {
+                                TreeInfo thisTree = trees[i];
+                                float dist = thisTree.location.distanceTo(rc.getLocation());
+                                if (dist < closest && thisTree.location.distanceTo(move) <= 1 + thisTree.radius) {
+                                    bugTree = thisTree.ID;
+                                    closest = dist;
+                                    treeOrNot=true;
+                                }
+                            }
+                            for (int i = robots.length - 1; i >= 0; i--) {
+                                RobotInfo thisTree = robots[i];
+                                float dist = thisTree.location.distanceTo(rc.getLocation());
+                                if (dist < closest && thisTree.location.distanceTo(move) <= 1 + thisTree.type.bodyRadius) {
+                                    bugTree = thisTree.ID;
+                                    closest = dist;
+                                    treeOrNot=false;
+                                }
+                            }
+                            if (closest==999f) {
+                                bugging=false;
+                            }
+                        }
                     }
                 }
             }

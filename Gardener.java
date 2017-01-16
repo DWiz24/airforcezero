2d614cc904dcb0cafcc1022a14e1b16dcfecbbf8
Ajay Fewell <ajayfewell@gmail.com>
2017-01-16 18:09:07 -0500

    Gardener actually waters now. If we have enough bullets to win by victory points, buys the victory points

--- Gardener.java
+++ Gardener.java
@@ -8,15 +8,27 @@ public class Gardener {
         	RobotType[] canBuild = {RobotType.SOLDIER, RobotType.LUMBERJACK, RobotType.TANK, RobotType.SCOUT};
         	int whichRobot = 0;
         	Random random = new Random();
-        	boolean buildTree = random.nextBoolean();
-        	MapLocation here = rc.getLocation();
-        	float x = here.x;
-        	float y = here.y;;
-    		Direction[] dirs={Direction.getNorth(),Direction.getSouth(),Direction.getEast(),Direction.getWest(), new Direction(here, new MapLocation(x+1, y+1))};
+    		Direction[] dirs={new Direction((float)(Math.PI/4.0)), Direction.getEast(), new Direction((float)(5.0*Math.PI/4.0)), Direction.getNorth(), new Direction((float)(3.0*Math.PI/4.0)),Direction.getSouth(),Direction.getWest()};
+    		MapLocation sad = null;
+    		float minhealth = 50f;
+    		TreeInfo[] trees = rc.senseNearbyTrees(2.0f);
+    		System.out.println(trees.length);
+    		for(TreeInfo tree:trees) {
+    			System.out.println("going through trees");
+    			if(tree.health < minhealth){
+    				minhealth = tree.health;
+    				sad = tree.location;
+    				System.out.println("watered a tree");
+    			}
+    		}
     		
+    		if(sad!=null && rc.canWater(sad))
+    			rc.water(sad);
     		if (rc.isBuildReady()) {
 				for (Direction place : dirs) {
-					if (rc.canPlantTree(place) && rc.isBuildReady() && buildTree) {
+					if(sad!=null && rc.isLocationOccupied(sad.add(place)))
+						continue;
+					if (rc.canPlantTree(place) && rc.isBuildReady()) {
 						System.out.println("about to build tree");
 						rc.plantTree(place);
 					}
@@ -24,9 +36,11 @@ public class Gardener {
 						System.out.println("about to build solider");
 						rc.buildRobot(canBuild[whichRobot], place);
 					}
-					buildTree = !buildTree;
 				}
 			}
+    		if(rc.getTeamBullets() >= 10000){
+    			rc.donate(10000f);
+    		}
             Clock.yield();
         }
     }

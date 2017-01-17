97ee01dc0dcc8466710968a2886cb953cd45a615
DWiz24 <bobthequark@gmail.com>
2017-01-17 10:53:10 -0500

    Exception are no longer fatal

--- RobotPlayer.java
+++ RobotPlayer.java
@@ -2,25 +2,31 @@ package airforcezero;
 import battlecode.common.*;
 
 public class RobotPlayer {
-    public static void run(RobotController rc) throws GameActionException{
-        switch(rc.getType()) {
-            case ARCHON:
-                Archon.run(rc);
-                break;
-            case GARDENER:
-                Gardener.run(rc);
-                break;
-            case LUMBERJACK:
-                Lumberjack.run(rc);
-                break;
-            case SCOUT:
-                Scout.run(rc);
-                break;
-            case SOLDIER:
-                Soldier.run(rc);
-                break;
-            case TANK:
-                Tank.run(rc);
+    public static void run(RobotController rc) throws GameActionException {
+        while (true) {
+            try {
+                switch (rc.getType()) {
+                    case ARCHON:
+                        Archon.run(rc);
+                        break;
+                    case GARDENER:
+                        Gardener.run(rc);
+                        break;
+                    case LUMBERJACK:
+                        Lumberjack.run(rc);
+                        break;
+                    case SCOUT:
+                        Scout.run(rc);
+                        break;
+                    case SOLDIER:
+                        Soldier.run(rc);
+                        break;
+                    case TANK:
+                        Tank.run(rc);
+                }
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
         }
     }
 }

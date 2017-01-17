7465a3479036384613423011444475545a6508af
Ajay Fewell <ajayfewell@gmail.com>
2017-01-16 21:12:18 -0500

    Gardener slightly better at choosing what to make

--- Gardener.java
+++ Gardener.java
@@ -5,36 +5,46 @@ public class Gardener {
     public static void run(RobotController rc) throws GameActionException {
         while(true){
         	
-        	RobotType[] canBuild = {RobotType.SOLDIER, RobotType.LUMBERJACK, RobotType.TANK, RobotType.SCOUT};
-        	int whichRobot = 0;
-        	Random random = new Random();
+        	//RobotType[] canBuild = {RobotType.SOLDIER, RobotType.LUMBERJACK, RobotType.TANK, RobotType.SCOUT};
+        	//int whichRobot = 0;
+
     		Direction[] dirs={new Direction((float)(Math.PI/4.0)), Direction.getEast(), new Direction((float)(5.0*Math.PI/4.0)), Direction.getNorth(), new Direction((float)(3.0*Math.PI/4.0)),Direction.getSouth(),Direction.getWest()};
-    		MapLocation sad = null;
+    		Random random = new Random();
+    		MapLocation sad = null; //Find lowest health tree, water it
     		float minhealth = 50f;
     		TreeInfo[] trees = rc.senseNearbyTrees(2.0f);
     		System.out.println(trees.length);
     		for(TreeInfo tree:trees) {
-    			System.out.println("going through trees");
     			if(tree.health < minhealth){
     				minhealth = tree.health;
     				sad = tree.location;
-    				System.out.println("watered a tree");
     			}
     		}
-    		
     		if(sad!=null && rc.canWater(sad))
     			rc.water(sad);
+    		
+    		//Build robots (just soldiers and lumberjacks atm) and plant trees 
+    		boolean buildtree = false;
+    		int numTrees = rc.getTreeCount();
+    		int numRobots = rc.getRobotCount();
+    		float nearbys = (rc.senseNearbyRobots(1.0f).length);
+    		System.out.println(nearbys);
+    		if(nearbys>3){
+    			Direction rand = new Direction((float)Math.random() * 2 * (float)Math.PI);
+    			if(rc.canMove(rand))
+    				rc.move(rand);
+    		}
+    		if(numTrees < numRobots/2)
+    			buildtree = true;
     		if (rc.isBuildReady()) {
 				for (Direction place : dirs) {
 					if(sad!=null && rc.isLocationOccupied(sad.add(place)))
 						continue;
-					if (rc.canPlantTree(place) && rc.isBuildReady()) {
-						System.out.println("about to build tree");
+					if (rc.canPlantTree(place) && rc.isBuildReady() && buildtree) { //Make sure this line actually works at some point
 						rc.plantTree(place);
 					}
 					if (rc.canBuildRobot(RobotType.SOLDIER, place) && rc.isBuildReady()) {
-						System.out.println("about to build solider");
-						rc.buildRobot(canBuild[whichRobot], place);
+						rc.buildRobot(RobotType.SOLDIER, place);
 					}
 				}
 			}

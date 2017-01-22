9a193a2aeafae8fd5de3fa33f7f3e9cadd63b6e5
Ajay Fewell <ajayfewell@gmail.com>
2017-01-22 00:32:13 -0500

    Gardeners much better in space, but still need work in tight areas

--- Gardener.java
+++ Gardener.java
@@ -3,8 +3,7 @@ import battlecode.common.*;
 public class Gardener {
     public static void run(RobotController rc) throws GameActionException {
     	final int roundSpawned = rc.getRoundNum();
-    	int soldiers = 0;
-    	int lumbers = 0;
+    	int soldiers = 0, lumbers = 0, planted = 0;    	
     	int channel = -1;
     	float theta = -1.0f;
         while(true){
@@ -40,6 +39,8 @@ public class Gardener {
     		int x = (int)myLocation.x;
     		int y = (int)myLocation.y;
     		int planting = 0b0000_0001;
+    		if(trees.length > 4)
+    			planting = 0b0000_0000;
     		int message = x;
     		message = (message << 12) + y;
     		message = (message << 12) + planting;
@@ -64,7 +65,7 @@ public class Gardener {
    			
     		//Moves to a good space for planting, based on more successful teams, changes turn to turn, stops trying after a certain number of turns
 			
-   			if(theta < 0.0f) {
+   			if(theta < -2.0f) { //For now, unable to move at all
    				
    				float deltaTheta = (float)(Math.PI/6.0);
    				
@@ -126,34 +127,35 @@ public class Gardener {
     		//End trying to move code
     		
     		//What do I build code
-    		int numTrees = rc.getTreeCount();
-    		int numRobots = rc.getRobotCount();
     		boolean buildtree = false;
     		
-	   		if(numTrees < numRobots && numRobots > 1)
+	   		if(planted == 4 || (planted < 5 && planted < soldiers*2 && soldiers > 1))
 	   			buildtree = true;
 	   		else
 	   			buildtree = false;
 	    		
 			for (Direction place : dirs) {
 				//if(sad!=null && rc.isLocationOccupied(sad.add(place)))
-				//	continue; 
+				System.out.println("direction" + place.radians);
+				System.out.println("buildtree" + buildtree);
+				System.out.println("canplant" + rc.canPlantTree(place));
 				if (rc.canPlantTree(place) && buildtree) { 
 					rc.plantTree(place);
+					planted++;
 				}
-				/*	if((rc.senseNearbyTrees(3f, Team.NEUTRAL).length > 4 || lumbers < soldiers/6 || baddirs > 3) && soldiers < 10) {
+					if((rc.senseNearbyTrees(3f, Team.NEUTRAL).length > 2) && soldiers < 2) {
 						if (rc.canBuildRobot(RobotType.LUMBERJACK, place) && rc.isBuildReady()) {
 							rc.buildRobot(RobotType.LUMBERJACK, place);
 							lumbers++;
 						}
-					}*/
+					}
 				
-					//else {
+					else {
 						if (rc.canBuildRobot(RobotType.SOLDIER, place) && rc.isBuildReady()) {
 							rc.buildRobot(RobotType.SOLDIER, place);
 							soldiers++;
 						}
-					//}
+					}
 					
 					
 			}

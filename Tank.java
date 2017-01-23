15980685fe655038a657b61ca6221665d7af2786
BrokenToaster1 <aleksey.didenkov@gmail.com>
2017-01-23 18:04:22 -0500

    Tanks actually suicide now
    while(false) would produce unreachable statements

--- Tank.java
+++ Tank.java
@@ -3,10 +3,10 @@ import battlecode.common.*;
 
 public class Tank {
     public static void run(RobotController rc) throws GameActionException {
-        while(false){
-            PublicMethods.donateBullets(rc);
-            Clock.yield();
-        }
+//        while(true){
+//            PublicMethods.donateBullets(rc);
+//            Clock.yield();
+//        }
     }
     public static void pickDest() {
     	
